dc0be6e76f25208b60fef6f4d2e75e1e9923359b
unmesh.g <unmesh.g@media.net>
2018-11-30 19:40:35 +0530

    google pwa

---
+++ service-worker.js
@@ -0,0 +1,66 @@
+var dataCacheName = 'abhisheksatreV1';
+var cacheName = 'abhisheksatreC-V1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/js/resume.js',
+  '/js/resume.min.js',
+  '/css/resume.css?v=124',
+  '/css/resume.min.css?v=124',
+  'images/logo.png',
+  'images/certificate.png',
+  'images/profile.png',
+  'images/profile.jpeg'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'http://127.0.0.1:8887/posts.json';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    console.log('cache the response');
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
